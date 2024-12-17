4932a3f6bcf62593f3a2dd5d46f571c205bf493a
youlimsongs <giuhki@gmail.com>
2024-12-18 00:26:20 +0900

    함수 분리

--- calculator.js
+++ calculator.js
@@ -5,9 +5,9 @@ const resultValue = document.getElementById("result");
 button.addEventListener("click", () => {
     const input = userInput.value.trim();
 
-    if (validateInput(input)) {
+    if (isValidInput(input)) {
         try {
-            const result = calculateInput(input);
+            const result = getCalculationResult(input);
             resultValue.textContent = `계산 결과: ${result}`;
         } catch (error) {
             resultValue.textContent = error.message;
@@ -17,18 +17,21 @@ button.addEventListener("click", () => {
     }
 });
 
-function validateInput(input) {
+const isValidInput = (input) => {
     const regex = /^[\d+\-*/.\s]+$/;
     return regex.test(input);
-}
+};
 
-
-function calculateInput(input) {
-    if(!input){
+const getCalculationResult = (input) => {
+    if (!input) {
         throw new TypeError("입력 값이 없습니다.");
     }
 
     const inputArray = input.split(' ');
+    return calculateInput(inputArray);
+};
+
+const calculateInput = (inputArray) => {
     let res = parseFloat(inputArray[0]);
 
     for (let i = 1; i < inputArray.length; i += 2) {
@@ -39,26 +42,26 @@ function calculateInput(input) {
             throw new TypeError("잘못된 수식입니다. 피연산자가 부족합니다.");
         }
 
-        switch (operator) {
-            case '+':
-                res += num;
-                break;
-            case '-':
-                res -= num;
-                break;
-            case '/':
-                if (num === 0) {
-                    throw new Error("0으로 나눌 수 없습니다.");
-                }
-                res /= num;
-                break;
-            case '*':
-                res *= num;
-                break;
-            default:
-                throw new RangeError("잘못된 연산자입니다.");
-        }
+        res = executeOperation(res, operator, num);
     }
 
     return res;
-}
+};
+
+const executeOperation = (res, operator, num) => {
+    switch (operator) {
+        case '+':
+            return res + num;
+        case '-':
+            return res - num;
+        case '/':
+            if (num === 0) {
+                throw new Error("0으로 나눌 수 없습니다.");
+            }
+            return res / num;
+        case '*':
+            return res * num;
+        default:
+            throw new RangeError("잘못된 연산자입니다.");
+    }
+};
